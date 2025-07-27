e471d814add4814898b99ad8c2c2a875b630cdc8
Florian Bussmann <hallo@florian-bussmann.de>
2025-07-27 14:58:54 +0000

    feat: add pwa manifest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+	plugins: [tailwindcss(), sveltekit(), SvelteKitPWA({
+		registerType: 'autoUpdate',
+		strategies: 'generateSW',
+		manifest: {
+			name: 'Svelte Vercel Demo',
+			short_name: 'svelte-vercel',
+			icons: [
+				{
+					src: '/icons/android-chrome-192x192.png',
+					sizes: '192x192',
+					type: 'image/png'
+				},
+				{
+					src: '/icons/android-chrome-512x512.png',
+					sizes: '512x512',
+					type: 'image/png'
+				}
+			]
+		},
+		devOptions: {
+			enabled: true
+		}
+	}),]
 });
